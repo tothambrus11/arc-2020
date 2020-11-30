07246b7457610c1d800f907a85f214615b817ff3
Starflame14 <martona149@gmai.com>
2020-11-30 11:45:26 +0100

    removed redundant part of DistFromLine

--- utvonaltervezo/src/hu/johetajava/pathfinding/DistFromLine.java
+++ utvonaltervezo/src/hu/johetajava/pathfinding/DistFromLine.java
@@ -2,32 +2,10 @@ package hu.johetajava.pathfinding;
 
 public class DistFromLine {
     double dist;
-    Position closest, lower_left, lower_right, upper_left, upper_right;
+    Position closest;
 
     DistFromLine(double dist, Position closest) {
         this.dist = dist;
         this.closest = closest;
-    }
-
-    DistFromLine(
-            double dist, Position closest,
-            Position lower_left, Position lower_right,
-            Position upper_left, Position upper_right
-    ) {
-        this.dist = dist;
-        this.closest = closest;
-        this.lower_left = lower_left;
-        this.lower_right = lower_right;
-        this.upper_left = upper_left;
-        this.upper_right = upper_right;
-    }
-
-    DistFromLine(DistFromLine distFromLine) {
-        dist = distFromLine.dist;
-        closest = distFromLine.closest;
-        lower_left = distFromLine.lower_left;
-        lower_right = distFromLine.lower_right;
-        upper_left = distFromLine.upper_left;
-        upper_right = distFromLine.upper_right;
     }
 }
